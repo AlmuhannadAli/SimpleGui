1a605ba63a1e2e52b765a87eb24ad1ea60678bd2
AlmuhannadALi <muhannad.aw@yahoo.com>
2023-03-07 06:49:06 +0200

    first commit

---
+++ Toellner.py
@@ -0,0 +1,19 @@
+import pyvisa
+from time import sleep 
+
+
+class toellner:
+
+    def __init__(self):
+        self.baudrate = 9600
+        self.timout = None
+        self.modelNumber = None 
+
+    def getCurrentVal():
+        None
+
+    def getVoltageVal(): 
+        None
+
+    def getPowerVal(): 
+        None
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import PySimpleGUI as sg
+
+# sg.Window(title="my GUI", layout=[[]], margins=(500, 250)).read()
